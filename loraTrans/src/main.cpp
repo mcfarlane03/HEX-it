adea157c0a62b89d965af720a56822582c883ec5
Shamari McPherson <mcphersonshamari1011@gmail.com>
2025-04-17 22:52:18 -0500

    struct added and 0 to 180 sweep

--- loraTrans/src/main.cpp
+++ loraTrans/src/main.cpp
@@ -5,6 +5,7 @@
  * @param incomingData The `incomingData` parameter in the `OnDataRecv` function is a pointer to the data received over the network. It is of type `const uint8_t*`, which means it is a pointer to an array of unsigned 8-bit integers (bytes) that represent the received data.
  * @param len The `len` parameter in the `OnDataRecv` function represents the length of the incoming data array that is being received. It indicates the size of the data being received from the sender device.
  */
+
 #include <Arduino.h>
 #include <Wire.h>        // Instantiate the Wire library
 #include <TFLI2C.h>      // TFLuna-I2C Library v.0.1.1
@@ -12,29 +13,29 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 #include <Adafruit_MPU6050.h>
-#include <ArduinoJson.h>
-
 #include <RadioLib.h>
 #include <esp_now.h>
 #include <WiFi.h>
+#include <vector>
 
+// Pin and constant definitions
 #define Vext 36
 #define SDA 33
 #define SCL 34
-
-#define ALTITUDE 1013.25
 
 #define DIO_1    14
 #define NSS      8
 #define RESET    12
 #define BUSY     13
 
-#define RXD2    3
-#define TXD2    36
-
-// #define LIDAR_FRONT_ADDR TFL_DEF_ADR  // Default address (0x10)
-// #define LIDAR_BACK_ADDR  0x11         // Second LiDAR address
 #define SERVO_PIN 45 
+#define ALTITUDE 1013.25
+
+// Scanning parameters
+#define ANGLE_STEP 5
+#define SCAN_POINTS 37  // (180/5 + 1)
+#define MIN_PASSABLE_GAP 20  // cm
+#define MIN_ANGLE_CHANGE 3   // degrees
 
 SX1262 radio = new Module(NSS, DIO_1, RESET, BUSY); // Create radio instance
 
@@ -45,17 +46,13 @@ Servo sweepServo;
 Adafruit_BMP280 bmp; // I2C
 Adafruit_MPU6050 mpu; // I2C
 
-int16_t  tfDist;    // distance in centimeters
-int16_t  tfAddr = TFL_DEF_ADR;  // Use this default I2C address
 
-bool personDetected = false;
-int txNumber = 0;
-String personDetectionJSON = "";  // to store camera messages
-
-// Servo sweep variables
+// Global variables
+int16_t tfDist; // distance in centimeters
+int16_t tfAddr = TFL_DEF_ADR; // Use this default I2C address
 int currentAngle = 0;
-bool sweepingUp = true;
-const int ANGLE_STEP = 6;  // Degrees to move per reading
+bool personDetected = false;
+bool sweepInProgress = false;
 
 // ESP-NOW configuration
 typedef struct struct_message {
@@ -63,6 +60,20 @@ typedef struct struct_message {
 } struct_message;
 
 struct_message cameraData;
+
+typedef struct SensorData {
+  uint32_t timestamp;
+  std::vector<int16_t> distances;
+  float temperature;           // Single temperature reading per sweep
+  std::vector<float> altitudes;
+  std::vector<float> accelX, accelY, accelZ;
+  std::vector<float> gyroX, gyroY, gyroZ;
+  std::vector<int16_t> angles;
+  bool humanDetected;
+} SensorData;
+
+SensorData data;
+
 
 // Function to handle incoming data from ESP-NOW
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
@@ -73,25 +84,6 @@ void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
   Serial.println(personDetected ? "DETECTED" : "NOT DETECTED");
 }
 
-// Function to update servo position
-void updateServoPosition() {
-  if (sweepingUp) {
-      currentAngle += ANGLE_STEP;
-      if (currentAngle >= 180) {
-          sweepingUp = false;
-          currentAngle = 180;
-      }
-  } else {
-      currentAngle -= ANGLE_STEP;
-      if (currentAngle <= 0) {
-          sweepingUp = true;
-          currentAngle = 0;
-      }
-  }
-  sweepServo.write(currentAngle);
-}
-
-
 void setup(){
     
     //Turn Vext on to power Sensors
@@ -99,7 +91,6 @@ void setup(){
     digitalWrite(Vext, LOW);
 
     Serial.begin(115200);  // Initalize serial port
-    
     Wire.begin(SDA, SCL);           // Initalize Wire library
 
     // Initialize servo
@@ -122,7 +113,8 @@ void setup(){
         Serial.println("Failed to find MPU6050 chip");
         while (1);
     }
-
+    
+     // Initialize LoRa
     Serial.print(F("[SX1262] Initializing ... "));
     int state = radio.begin();
     if (state == RADIOLIB_ERR_NONE) {
@@ -133,6 +125,7 @@ void setup(){
       while (true) { delay(10); }
     }
 
+    // Initialize ESP-NOW
     WiFi.mode(WIFI_STA);
     if (esp_now_init() != ESP_OK) {
         Serial.println("ESP-NOW init failed");
@@ -144,70 +137,83 @@ void setup(){
 
 }
 
-void loop(){
+void loop() {
+  if (currentAngle == 0) {
+      // Start new sweep
+      sweepInProgress = true;
+      // Take initial temperature reading
+      data.temperature = bmp.readTemperature();
+  }
 
-    personDetected = false;
-
-    updateServoPosition(); // Move the servo to the next position
-
-    tflI2C.getData( tfDist, tfAddr);
-    sensors_event_t a, g, temp;
-    mpu.getEvent(&a, &g, &temp);
-
-    // 3. JSON document
-
-    JsonDocument doc;
-    // doc["DistanceFront"] = distFront;
-    // doc["DistanceBack"] = distBack;
-    doc["Timestamp"] = millis();
-    doc["Distance"] = tfDist;
-    doc["Temp"] = bmp.readTemperature();
-    doc["Pressure"] = bmp.readPressure();
-    doc["Alti"] = bmp.readAltitude(ALTITUDE);
-    doc["Accel_X"] = a.acceleration.x;
-    doc["Accel_Y"] = a.acceleration.y;
-    doc["Accel_Z"] = a.acceleration.z;
-    doc["Rotat_X"] = g.gyro.x;
-    doc["Rotat_Y"] = g.gyro.y;
-    doc["Rotat_Z"] = g.gyro.z;
-    doc["Angle"] = currentAngle; 
-    doc["Sweep"] = sweepingUp ? "up" : "down";   
-    doc["Human"] = personDetected;
-     
-
-   // 4. Send data over LoRa
-    String jsonString;
-    serializeJson(doc, jsonString);
-
-    // serializeJson(doc, Serial);
-    // Serial.println("");
-
-    int state = radio.transmit(jsonString.c_str(), jsonString.length());
-
-    if (state == RADIOLIB_ERR_NONE) {
-      // the packet was successfully transmitted
-      Serial.println(F("success!"));
+  // Take sensor readings
+  tflI2C.getData(tfDist, tfAddr);
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
   
+  // Store readings in vectors
+  data.distances.push_back(tfDist);
+  data.angles.push_back(currentAngle);
+  data.altitudes.push_back(bmp.readAltitude(ALTITUDE));
+  data.accelX.push_back(a.acceleration.x);
+  data.accelY.push_back(a.acceleration.y);
+  data.accelZ.push_back(a.acceleration.z);
+  data.gyroX.push_back(g.gyro.x);
+  data.gyroY.push_back(g.gyro.y);
+  data.gyroZ.push_back(g.gyro.z);
+
+  // Move to next angle
+  currentAngle += ANGLE_STEP;
+  sweepServo.write(currentAngle);
+
+  // Check if sweep complete
+  if (currentAngle > 180 && sweepInProgress) {
+      // Use personDetected status as is - it will be true if person was detected at any point
+      data.humanDetected = personDetected;
+      data.timestamp = millis();
+        
+      // Transmit complete sweep data
+      int state = radio.transmit((uint8_t*)&data, sizeof(data));
+        
+      if (state == RADIOLIB_ERR_NONE) {
+          Serial.println(F("Sweep data transmitted successfully"));
+          Serial.printf("Points collected: %d\n", data.distances.size());
+
       // print measured data rate
       Serial.print(F("[SX1262] Datarate:\t"));
       Serial.print(radio.getDataRate());
       Serial.println(F(" bps"));
   
-    } else if (state == RADIOLIB_ERR_PACKET_TOO_LONG) {
+  } else if (state == RADIOLIB_ERR_PACKET_TOO_LONG) {
       // the supplied packet was longer than 256 bytes
       Serial.println(F("too long!"));
   
-    } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
-      // timeout occured while transmitting packet
+  } else if (state == RADIOLIB_ERR_TX_TIMEOUT) {
+      // timeout occurred while transmitting packet
       Serial.println(F("timeout!"));
   
-    } else {
+  } else {
       // some other error occurred
       Serial.print(F("failed, code "));
       Serial.println(state);
+  }
   
-    }
+  // Reset for next sweep
+  currentAngle = 0;
+  sweepServo.write(0);
+  sweepInProgress = false;
   
+  // Clear vectors for next sweep
+  data.distances.clear();
+  data.angles.clear();
+  data.altitudes.clear();
+  data.accelX.clear();
+  data.accelY.clear();
+  data.accelZ.clear();
+  data.gyroX.clear();
+  data.gyroY.clear();
+  data.gyroZ.clear();
+      
+  delay(100);
+  }
 
-    delay(500);
 }
